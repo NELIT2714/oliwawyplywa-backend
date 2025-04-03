4592977687dace28c9e4cb469425e0b5fd2894bc
NELIT2714 <nelit2714@gmail.com>
2025-04-03 23:59:25 +0200

    feat: add ShippingAddresses repository

--- src/main/java/pl/oliwawyplywa/web/repositories/ShippingAddressesRepository.java
+++ src/main/java/pl/oliwawyplywa/web/repositories/ShippingAddressesRepository.java
@@ -1,4 +1,10 @@
 package pl.oliwawyplywa.web.repositories;
 
-public interface ShippingAddressesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.oliwawyplywa.web.schemas.ShippingAddress;
+
+@Repository
+public interface ShippingAddressesRepository extends JpaRepository<ShippingAddress, Integer> {
+
 }
