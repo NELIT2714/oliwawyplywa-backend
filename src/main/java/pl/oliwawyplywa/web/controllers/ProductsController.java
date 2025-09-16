776fd0284f59466f53b67ac19120644fa18660b8
NELIT2714 <nelit2714@gmail.com>
2025-09-16 23:12:31 +0200

    docs: add docs for product post endpoint

--- src/main/java/pl/oliwawyplywa/web/controllers/ProductsController.java
+++ src/main/java/pl/oliwawyplywa/web/controllers/ProductsController.java
@@ -2,12 +2,14 @@ package pl.oliwawyplywa.web.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import pl.oliwawyplywa.web.dto.products.CreateProductDTO;
 import pl.oliwawyplywa.web.dto.products.ResponseProductDTO;
+import pl.oliwawyplywa.web.schemas.Category;
 import pl.oliwawyplywa.web.services.ProductsService;
 import pl.oliwawyplywa.web.utils.mappers.ProductMapper;
 import reactor.core.publisher.Flux;
@@ -56,7 +58,7 @@ public class ProductsController {
                             ]
                           }
                         ]
-                        """
+                    """
             )
         )
     )
@@ -65,6 +67,68 @@ public class ProductsController {
             .flatMap(productMapper::mapProductToDTO);
     }
 
+    @Operation(
+        summary = "Create product",
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            required = true,
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Category.class),
+                examples = {
+                    @ExampleObject(
+                        name = "Create example product",
+                        value =
+                            """
+                            {
+                              "category_id": 1,
+                              "product_name": "Test product",
+                              "product_description": "Test description",
+                              "product_options": [
+                                {
+                                  "option_label": "Option name 1",
+                                  "option_price": 10.00
+                                },
+                                {
+                                  "option_label": "Option name 2",
+                                  "option_price": 20.00
+                                }
+                              ]
+                            }
+                            """
+                    )
+                }
+            )
+        )
+    )
+    @ApiResponse(
+        responseCode = "200",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(
+                example =
+                    """
+                    {
+                      "product_id": 1,
+                      "category_id": 1,
+                      "product_name": "Test product",
+                      "product_description": "Test description",
+                      "product_options": [
+                        {
+                          "option_id": 1
+                          "option_label": "Option name 1",
+                          "option_price": 10.00
+                        },
+                        {
+                          "option_id": 2
+                          "option_label": "Option name 2",
+                          "option_price": 20.00
+                        }
+                      ]
+                    }
+                    """
+            )
+        )
+    )
     @PostMapping
     public Mono<ResponseProductDTO> createProduct(@RequestBody CreateProductDTO productDTO) {
         return productsService.createProduct(productDTO)
